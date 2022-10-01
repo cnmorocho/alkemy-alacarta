54bb9412a830c37a6a59fd1663c9a1af75357eed
Carlos Nahuel Morocho <nahuelmorocho@gmail.com>
2022-09-30 21:31:43 -0300

    Se agrego la página de inicio de sesión a las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginFormComponent } from './auth/components/login-form/login-form.component';
 import { AuthGuard } from './auth/guards/auth.guard';
-import { HomeComponent } from './core/pages/home/home.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginFormComponent },
-  { path: '', component: HomeComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
