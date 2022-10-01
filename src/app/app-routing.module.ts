652f7b0332d41279c758a5c2ddd0072a9e7da4b7
Carlos Nahuel Morocho <nahuelmorocho@gmail.com>
2022-09-30 21:25:17 -0300

    Se agrego home a las rutas y se lo protegió con el auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './auth/components/login-form/login-form.component';
+import { AuthGuard } from './auth/guards/auth.guard';
+import { HomeComponent } from './core/pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginFormComponent },
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
