4de3f9124645087e7fb158decb5fbe228ecd89ee
Carlos Nahuel Morocho <nahuelmorocho@gmail.com>
2022-10-07 17:51:15 -0300

    Se agregaron a las rutas, el homepage y la listpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginFormComponent } from './auth/components/login-form/login-form.component';
 import { AuthGuard } from './auth/guards/auth.guard';
+import { HomePageComponent } from './core/pages/home-page/home-page.component';
+import { ListPageComponent } from './core/pages/list-page/list-page.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginFormComponent },
+  { path: '', component: HomePageComponent, canActivate: [AuthGuard] },
+  { path: 'list', component: ListPageComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
