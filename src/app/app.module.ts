9c89a117087b9ec04882bbba97e3c9b24f876f30
Carlos Nahuel Morocho <nahuelmorocho@gmail.com>
2022-10-07 17:50:19 -0300

    Se agregaron FormsModule y ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,29 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AuthModule } from './auth/auth.module';
+import { MenuItemComponent } from './core/components/menu-item/menu-item.component';
+import { HomePageComponent } from './core/pages/home-page/home-page.component';
+import { ListPageComponent } from './core/pages/list-page/list-page.component';
+import { SearchBarComponent } from './core/components/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MenuItemComponent,
+    HomePageComponent,
+    ListPageComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AuthModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
