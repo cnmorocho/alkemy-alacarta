c0ef50c0aed2b87f2aaac998539d49ecdc97dc15
Carlos Nahuel Morocho <nahuelmorocho@gmail.com>
2022-10-12 17:05:31 -0300

    Se agrego el AuthModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AppRoutingModule } from '../app-routing.module';
+import { AuthModule } from '../auth/auth.module';
 
 @NgModule({
   declarations: [NavbarComponent],
-  imports: [CommonModule, AppRoutingModule],
+  imports: [CommonModule, AppRoutingModule, AuthModule],
   exports: [NavbarComponent],
 })
 export class SharedModule {}
