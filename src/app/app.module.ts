05a6b71bdab1aadf902df7bc3e83aac2a595e1d0
Carlos Nahuel Morocho <nahuelmorocho@gmail.com>
2022-10-13 19:58:57 -0300

    Se declaro MenuItem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,23 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AuthModule } from './auth/auth.module';
-import { MenuItemComponent } from './core/components/menu-item/menu-item.component';
+import { ListItemComponent } from './core/components/list-item/list-item.component';
 import { HomePageComponent } from './core/pages/home-page/home-page.component';
 import { ListPageComponent } from './core/pages/list-page/list-page.component';
 import { SearchBarComponent } from './core/components/search-bar/search-bar.component';
 import { FoodDetailPageComponent } from './core/pages/food-detail-page/food-detail-page.component';
 import { SharedModule } from './shared/shared.module';
+import { MenuItemComponent } from './core/components/menu-item/menu-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuItemComponent,
+    ListItemComponent,
     HomePageComponent,
     ListPageComponent,
     SearchBarComponent,
     FoodDetailPageComponent,
+    MenuItemComponent,
   ],
   imports: [
     BrowserModule,
