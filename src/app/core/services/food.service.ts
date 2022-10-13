f998723f903f3048aa89780b8eb7696db7ca4048
Carlos Nahuel Morocho <nahuelmorocho@gmail.com>
2022-10-13 16:26:15 -0300

    Se rehicieron los metodos utilizando la clase Menu

--- src/app/core/services/food.service.ts
+++ src/app/core/services/food.service.ts
@@ -6,13 +6,14 @@ import { map } from 'rxjs/operators';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { Response } from '../models/ResponseApi.interface';
 import { Food } from '../models/ResponseApi.interface';
+import { Menu } from '../models/Menu';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FoodService {
   private url: string = 'https://api.spoonacular.com/recipes';
-  menu: Food[] = [];
+  menu: Menu = new Menu();
 
   // ! Luego usar el api key proporcionado por el AuthService
   private token: string = '5f66ee2aeff844a8b3afb81a230c1e7d';
@@ -33,19 +34,15 @@ export class FoodService {
     );
   }
 
-  // TODO: Comprobar que hallan al menos 2 platos veganos
   public addToMenu(food: Food) {
-    if (this.menu.length <= 3 && food.vegan) {
-      const veganPlates = this.menu.filter((foodOnMenu) => foodOnMenu.vegan);
-      if (veganPlates.length <= 1) this.menu.push(food);
-    } else if (this.menu.length <= 3) this.menu.push(food);
+    this.menu.addToMenu(food);
   }
 
-  public removeFromMenu(id: number) {
-    this.menu = this.menu.filter((elem) => elem.id == id);
+  public removeFromMenu(id: number): void {
+    this.menu.removeFromMenu(id);
   }
 
-  public getMenu(): Food[] {
+  public getMenu(): Menu {
     return this.menu;
   }
 }
